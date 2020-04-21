45f86fa8e056f35736e7fd3137d0a8eaca107a92
benoitGaly64 <34302380+benoitGaly64@users.noreply.github.com>
2020-04-21 14:31:19 +0200

    Create Layout and Navbar

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,12 @@
+
+    <!-- Sidebar -->
+
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">Mon agence immo</div>
+      <div class="list-group list-group-flush">
+
+                  <a href="/home" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+ 
+      </div>
+    </div>
+    <!-- /sidebar-wrapper -->
---
+++ resources/views/inc/subnavbar.blade.php
@@ -0,0 +1,46 @@
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+    <button class="btn btn-primary" id="menu-toggle"><i id="menu-toggle" class="fas fa-bars"></i></button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link"
+                        href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if(Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                @if(Route::has('login'))
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }}</span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              {{ csrf_field() }}
+                            </form>
+                    </li>
+                @endif
+            @endif
+
+        </ul>
+
+    </div>
+</nav> 
+
+
